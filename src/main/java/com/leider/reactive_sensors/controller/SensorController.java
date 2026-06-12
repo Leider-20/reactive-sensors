98f674667564f2a8151c703b62a0afedb31816ba
LeiderF <leider.caicedo@udea.edu.co>
2026-06-12 00:27:23 -0500

    Files are updated

--- src/main/java/com/leider/reactive_sensors/controller/SensorController.java
+++ src/main/java/com/leider/reactive_sensors/controller/SensorController.java
@@ -7,12 +7,10 @@ import com.leider.reactive_sensors.service.AlertService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping
 @RequiredArgsConstructor
 public class SensorController {
 
